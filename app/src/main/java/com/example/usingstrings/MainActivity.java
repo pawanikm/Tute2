ba33118c181023ef0b5aeb4b8e6a3654475cf13d
pawanikm <pawanimunasinghe99@gmail.com>
2020-08-07 15:47:58 +0530

    Initial commit

---
+++ app/src/main/java/com/example/usingstrings/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.usingstrings;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView txtMsg2 = findViewById(R.id.tvMsg2);
+        txtMsg2.setText(R.string.Msg2);
+
+        Log.i("Lifecycle", "OnCreate called..");
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.i("Lifecycle", "OnStart called..");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+        Log.i("Lifecycle", "OnRestart called..");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.i("Lifecycle", "OnResume called..");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.i("Lifecycle", "OnPause called..");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.i("Lifecycle", "OnStop called..");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.i("Lifecycle", "OnDestroy called..");
+    }
+
+}
